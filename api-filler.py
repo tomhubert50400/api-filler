c8cf28b14a070f8628cdb3c12e4c57c51d07eb69
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2021-02-12 09:26:39 +0100

    Update api-filler.py

--- api-filler.py
+++ api-filler.py
@@ -1,13 +1,5 @@
 # coding: utf-8
 
-echo "# api-filler.py" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/tomhubert50400/api-filler.py.git
-git push -u origin main
-
 import time
 import requests
 from random import seed
@@ -16,13 +8,13 @@ from random import random
 seed(1)
 # generate random numbers between 0-1
 while True:
-        res = requests.get('http://api.weatherstack.com/current?access_key=7f4390e1e5c441bce30e182c61227707&query=Rouen')
-        res = res.json()
-        temp = res["current"]["temperature"]
-        humidity = res["current"]["humidity"]
-        parameters = {"temperature": temp, "humidity": humidity}
-        print(parameters)
-        res = requests.post('http://192.168.202.252:8080/create', json={"temperature": temp, "humidity": humidity})
-        res = res.json()
-        print(res)
-        time.sleep(60*5)
+    res = requests.get('http://api.weatherstack.com/current?access_key=7f4390e1e5c441bce30e182c61227707&query=Rouen')
+    res = res.json()
+    temp = res["current"]["temperature"]
+    humidity = res["current"]["humidity"]
+    parameters = {"temperature": temp, "humidity": humidity}
+    print(parameters)
+    res = requests.post('http://192.168.202.252:8080/create', json=parameters, headers={"Authorization": "test"})
+    res = res.json()
+    print(res)
+    time.sleep(60*5)
